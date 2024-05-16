240930b00c0beda8526a513efab209ed9fbf1dfd
manognachennuru <manognachennuru@gmail.com>
2024-05-16 16:45:45 -0400

    modified journey.js

--- src/components/Journey.js
+++ src/components/Journey.js
@@ -20,16 +20,18 @@ export const Journey = () => {
                     <img src={adpLogo} alt="ADP Logo" className="curved-img" />
                   </Col>
                   <Col xs={12} sm={9}>
-                    <h3>Site Reliability Engineer</h3>
-                    <p>Automatic Data Processing (ADP), Hyderabad, India</p>
+                    <h3>Software Engineer</h3>
+                    <p>Automatic Data Processing (ADP)</p>
                     <ul>
-                      <li>Served as a <strong>Site Reliability Engineer (SRE)</strong> for ADP Vantage HCM, a robust payroll management solution designed for multinational corporations with over 1000 employees.</li>
-                      <li>Implemented Automated End-to-End Continuous Build, Integration, Delivery, Release, and Deployment processes.</li>
-                      <li>Led the implementation of a Webex integration for a Change Order (CO) management project, resulting in a substantial increase in productivity across the entire NAS VANTAGE TEAM, which consists of over a hundred employees.</li>
-                      <li>Involved in the migration of infrastructure, transitioning from in-house virtual machines to <strong>Amazon Web Services</strong> EC2 hosts, EKS Clusters (Elastic Kubernetes Service), and Docker containers.</li>
-                      <li>Developed CI/CD pipelines to facilitate deployment of user interface (UI) and database (DB) deployments.</li>
+                      <li>Utilized ReactJS and Redux to refactor and modernize the user interface of the <strong> ADP Vantage HCM payroll system, </strong> implementing responsive design principles and state management techniques, consequently reducing user interaction time by 20% in the payroll management module.</li>
+                      <li>Leveraged Spring Boot to enhance backend services, resulting in a 30% reduction in average response time for payroll processing requests.</li>
+                      <li>Integrated OAuth2 with Spring Security to implement robust authentication and authorization mechanisms, ensuring secure access to sensitive payroll data.</li>
+                      <li>Implemented comprehensive unit testing using JUnit and Mockito to validate the functionality and reliability of the enhanced payroll system across various use cases.</li>
+                      <li>Collaborated with cross-functional teams to implement Docker for containerization and Jenkins for continuous integration and delivery, resulting in a streamlined deployment process and enhanced overall system reliability.</li>
+                      <li>Played a key role in the migration of critical payroll system components to AWS cloud infrastructure, actively utilizing services such as AWS EC2, RDS, and Lambda, resulting in a 20% reduction in infrastructure costs, contributing to improved scalability.</li>
                       <li>Recognized with the <strong> Employee of the Month </strong> award (August 2022) for outstanding contributions to the project.</li>
                     </ul>
+                    
 
                   </Col>
                 </Row>
@@ -40,20 +42,16 @@ export const Journey = () => {
                     <img src={uncLogo} alt="UNC Logo" className="curved-img" />
                   </Col>
                   <Col xs={12} sm={9}>
-                    <h3>Teaching Assistant (Part time)</h3>
-                    <p>UNC Charlotte, NC, USA</p>
-                    <p>Course ITCS 6100 - Big Data Analytics for Competitive Advantage under: Dr. Pamela Thompson.</p>
+                    <h3>Graduate Assistant (Part time)</h3>
+                    <p>UNC Charlotte</p>
+                    {/* <p>Course ITCS 6100 - Big Data Analytics for Competitive Advantage under: Dr. Pamela Thompson.</p> */}
                     <div>
-                      <ul>
-                        <li>Provided instructional support and hands-on expertise with Amazon Web Services (AWS).</li>
-                        <li>Engaged in course project development and grading assignment submissions for 100 students.</li>
-                      </ul>
-                      <p>Course ITCS 4111/5111 - Introduction to Natural Language Processing under: Dr. Razvan Bunescu.</p>
-                      <ul>
-                        <li>Collaborated in the development of course materials and assignments catering to both undergraduate and master's students.</li>
-                        <li>Conducted two weekly office hours, aiding students with NLP queries and assignment comprehension.</li>
-                        <li>Employed <strong>Python</strong> proficiency with NumPy, Pandas, Spacy, NLTK, TensorFlow and Chat Completion API.</li>
-                      </ul>
+                    <ul>
+                      <li>Collaborated extensively with the course instructor to develop comprehensive course materials and assignments tailored for undergraduate and master's students, ensuring alignment with curriculum objectives and academic standards.</li>
+                      <li>Demonstrated proficiency in both Java and Python programming languages while grading student research projects, ensuring comprehensive assessment and feedback.</li>
+                      <li>Provided hands-on assistance to students in implementing full-stack projects, encompassing frontend technologies such as React JS and Angular JS, backend technologies including Spring Boot and Node.js, along with database management systems like PostgreSQL and MySQL.</li>
+                      <li>Actively participated in code reviews and provided constructive feedback to improve code quality and maintainability, along with, offering guidance in unit testing practices to ensure project robustness and successful completion.</li>
+                    </ul>
                     </div>
                   </Col>
                 </Row>
@@ -64,8 +62,8 @@ export const Journey = () => {
                     <img src={oneit} alt="oneit" className="curved-img" />
                   </Col>
                   <Col xs={12} sm={9}>
-                    <h3>Desktop support technician (Part time)</h3>
-                    <p>UNC Charlotte OneIT office, Charlotte, NC, USA</p>
+                    <h3>Desktop Support Technician (Part time)</h3>
+                    <p>UNC Charlotte OneIT office, Charlotte</p>
                     <ul>
                       <li>Effectively resolved software-related issues across the campus, ensuring the resolution of reported tickets on the OneIT website. </li>
                       <li>Managed the installation, setup, and configuration of newly acquired devices within the campus.</li>
